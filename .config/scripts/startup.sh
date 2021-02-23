6721a1eacc0be53a7a1946de82985ca9ad796d56
r4v3n6101 <raven6107@gmail.com>
2021-02-23 04:41:48 +0300

    Fix Xresources loading

--- .config/scripts/startup.sh
+++ .config/scripts/startup.sh
@@ -1,6 +1,7 @@
+xrdb ~/.config/xorg/.Xresources
 setxkbmap -layout us,ru -option 'grp:alt_shift_toggle'
 feh --bg-fill ~/.config/wallpaper/bg.jpg &
 compton -b -f &
-sh ~/.config/scripts/launch_polybar.sh
-dex -a &
 dunst &
+sh ~/.config/scripts/launch_polybar.sh
+dex -a -s ~/.config/autostart/ &
