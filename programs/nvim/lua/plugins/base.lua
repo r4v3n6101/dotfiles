713258a3edf5e3db5173e52a0a781e70b75d728e
r4v3n6101 <raven6107@gmail.com>
2024-07-30 19:09:51 +0300

    update

--- programs/nvim/lua/plugins/base.lua
+++ programs/nvim/lua/plugins/base.lua
@@ -3,8 +3,33 @@ return {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
-        config = function()
-            vim.cmd([[colorscheme catppuccin-frappe]])
+        opts = {
+            no_italic = true,
+            term_colors = true,
+            transparent_background = false,
+            styles = {
+                comments = {},
+                conditionals = {},
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+            },
+            color_overrides = {
+                mocha = {
+                    base = "#000000",
+                    mantle = "#000000",
+                    crust = "#000000",
+                },
+            },
+        },
+        init = function()
+            vim.cmd([[colorscheme catppuccin]])
         end
     },
 
