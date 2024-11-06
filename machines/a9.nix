266da928883db589e45aee8bc34510028e21ff44
r4v3n6101 <raven6107@gmail.com>
2024-11-06 21:59:49 +0300

    Update a9.nix

--- machines/a9.nix
+++ machines/a9.nix
@@ -30,7 +30,19 @@
     yggdrasil = {
       enable = true;
       persistentKeys = true;
-      settings = { Peers = [ "tcp://srv.itrus.su:7991" ]; };
+      settings = {
+        Peers = [
+          # Russia
+          "tls://s-mow-1.sergeysedoy97.ru:65534"
+          "quic://srv.itrus.su:7993"
+          # Germany
+          "quic://ygg-uplink.thingylabs.io:443"
+          "tcp://193.107.20.230:7743"
+          # Netherlands
+          "quic://vpn.itrus.su:7993"
+          "ws://vpn.itrus.su:7994"
+        ];
+      };
     };
     logind.lidSwitch = "ignore";
   };
