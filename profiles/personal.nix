12c0acab7ad17b17a3471ed2e0e131f0254194e6
r4v3n6101 <raven6107@gmail.com>
2026-02-11 18:16:54 +0300

    television: basic configuration

--- profiles/personal.nix
+++ profiles/personal.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   lib,
+  config,
   inputs,
   ...
 }:
@@ -13,11 +14,11 @@
     stateVersion = "25.05";
     packages = with pkgs; [
       # Man pages
-      tldr
       man-pages
       man-pages-posix
 
       # Main utils
+      xdg-utils
       nmap
       iperf
       tree
@@ -50,13 +51,88 @@
 
     man = {
       enable = true;
-      generateCaches = false;
+      generateCaches = true;
+    };
+
+    tealdeer = {
+      enable = true;
+      enableAutoUpdates = true;
     };
 
     television = {
       enable = true;
       enableBashIntegration = true;
       enableFishIntegration = true;
+      settings.shell_integration.channel_triggers = {
+        tldr = [ "tldr" ];
+        git-branch = [
+          "git checkout"
+        ];
+        git-diff = [
+          "git add"
+          "git restore"
+        ];
+        git-log = [
+          "git log"
+          "git show"
+        ];
+        nixpkgs = [
+          "nix shell"
+          "nix run"
+          "nix develop"
+          "nix profile add"
+        ];
+      };
+      channels = {
+        man-pages = {
+          metadata = {
+            name = "man-pages";
+            description = "Browse and preview system manual pages";
+            requirements = [
+              "apropos"
+              "man"
+            ];
+          };
+          source.command = "apropos .";
+          preview = {
+            command = "man '{0}'";
+            env = {
+              "MANWIDTH" = "80";
+            };
+          };
+          keybindings.enter = "actions:open";
+          actions.open = {
+            description = "Open the selected man page in the system pager";
+            command = "man '{0}'";
+            mode = "execute";
+          };
+        };
+        nixpkgs = {
+          metadata = {
+            name = "nixpkgs";
+            description = "Search nixpkgs";
+            requirements = [
+              "sed"
+              "nix-search-tv"
+              "xdg-open"
+            ];
+          };
+          # Retain only packages, not options
+          source.command = [
+            "nix-search-tv print | grep -E '^nixpkgs' | sed 's|/ |#|'"
+            "nix-search-tv print | grep -E '^nur' | sed 's|/ |#|'"
+          ];
+          preview.command = "nix-search-tv preview --indexes {split:#:0} '{split:#:1}'";
+          actions.homepage = {
+            description = "Open homepage";
+            command = "nix-search-tv homepage --indexes {split:#:0} '{split:#:1}' | xargs xdg-open";
+          };
+          actions.source = {
+            description = "Open source";
+            command = "nix-search-tv source --indexes {split:#:0} '{split:#:1}' | xargs xdg-open";
+          };
+        };
+      };
     };
 
     nix-search-tv = {
