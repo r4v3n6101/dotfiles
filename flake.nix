6cf231343805b562298f08bdeee8d5a389e62520
r4v3n6101 <raven6107@gmail.com>
2026-03-02 03:16:49 +0300

    update servers ports

--- flake.nix
+++ flake.nix
@@ -17,7 +17,10 @@
       url = "github:LnL7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    virby.url = "github:quinneden/virby-nix-darwin/be170bd7ef21ce9773e7daa646d43f5405a1bdb2";
+    virby = {
+      url = "github:quinneden/virby-nix-darwin/be170bd7ef21ce9773e7daa646d43f5405a1bdb2";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     mac-app-util.url = "github:hraban/mac-app-util";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
